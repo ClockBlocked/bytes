1d609a839b43a9234ccf517cfc7b63fe4be59e05
Cole Hanson <4theloveofcarl@gmail.com>
2025-12-29 16:23:39 -0600

    update modules/coder.js

--- modules/coder.js
+++ modules/coder.js
@@ -38,148 +38,176 @@ class coderViewEdit {
     this.isInitialized = true;
   }
 
-  getTemplate() {
-    return `
-
+getTemplate() {
+  return `
 <!-- data-page="file" ---------------->
-    <div class="container">
-      <div class="fileHeader"><!-- Header  -->
-      
-  <!-- Header  |  Left -->
-        <div class="fileActions">
-          <button id="editSaveBtn" class="actionButton" title="Edit">
-            <svg id="editSaveIcon" class="icon" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M11.013 1.427a1.75 1.75 0 0 1 2.474 0l1.086 1.086a1.75 1.75 0 0 1 0 2.474l-8.61 8.61a1.75 1.75 0 0 1-.757.437l-3.26.88a.75.75 0 0 1-.918-.918l.88-3.26a1.75 1.75 0 0 1 .437-.757l8.61-8.61Zm1.414 1.06a.25.25 0 0 0-.354 0L11.26 3.3l1.44 1.44 1.113-1.113a.25.25 0 0 0 0-.354l-1.086-1.086Z"></path>
-            </svg>
-          </button>
-          <button id="cancelBtn" class="actionButton hide" title="Cancel">
-            <svg class="icon" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M3.72 3.72a.75.75 0 0 1 1.06 0L8 6.94l3.22-3.22a.75.75 0 1 1 1.06 1.06L9.06 8l3.22 3.22a.75.75 0 1 1-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 0 1-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 0 1 0-1.06Z"></path>
-            </svg>
-          </button>
-          <button id="copyBtn" class="actionButton" title="Copy">
-            <svg class="icon" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v6.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 15h-7.5A1.75 1.75 0 0 1 0 13.25v-6.5Zm5-5A1.75 1.75 0 0 0 3.25 3.5v6.5A1.75 1.75 0 0 0 5 11.75h7.5A1.75 1.75 0 0 0 14.25 10V3.5A1.75 1.75 0 0 0 12.5 1.75H5Zm.25 1.5a.25.25 0 0 0-.25.25v6.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-6.5a.25.25 0 0 0-.25-.25H5.25Z"></path>
-            </svg>
-          </button>
-          <button id="downloadBtn" class="actionButton" title="Download">
-            <svg class="icon" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M2.75 14A1.75 1.75 0 0 1 1 12.25v-2.5a.75.75 0 0 1 1.5 0v2.5c0 .138.112.25.25.25h10.5a.25.25 0 0 0 .25-.25v-2.5a.75.75 0 0 1 1.5 0v2.5A1.75 1.75 0 0 1 13.25 14H2.75Zm3.5-5.75a.75.75 0 0 1 1.5 0v-6.5a.75.75 0 0 1 1.5 0v6.5a.75.75 0 0 1 1.5 0l-2.25 2.5a.75.75 0 0 1-1.06 0l-2.24-2.5Z"></path>
-            </svg>
-          </button>
-        </div>
-
-
-
-  <!-- Header  |  Right -->
-        <div class="toolbarGroup">
-          <button id="themeToggleBtn" class="toolbarButton" title="Toggle Theme">
-            <svg id="themeIcon" class="smallIcon" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M8 4a4 4 0 1 1 0 8 4 4 0 0 1 0-8Z"></path>
-            </svg>
-          </button>
-          <div data-toolbar="fontSize" class="fontSizeControl">
-            <button id="decreaseFontBtn" class="fontButton">
-              <svg class="tinyIcon" fill="currentColor" viewBox="0 0 16 16">
-                <path d="M3.75 7.25a.75.75 0 0 0 0 1.5h8.5a.75.75 0 0 0 0-1.5Z"></path>
-              </svg>
-            </button>
-            <span id="fontSizeDisplay" class="fontSizeDisplay">12px</span>
-            <button id="increaseFontBtn" class="fontButton">
-              <svg class="tinyIcon" fill="currentColor" viewBox="0 0 16 16">
-                <path d="M7.25 3.75a.75.75 0 0 1 1.5 0V7.25h3.5a.75.75 0 0 1 0 1.5h-3.5v3.5a.75.75 0 0 1-1.5 0v-3.5h-3.5a.75.75 0 0 1 0-1.5h3.5Z"></path>
-              </svg>
-            </button>
-          </div>
-          <button id="wrapLinesBtn" class="toolbarButton" title="Wrap Lines">
-            <svg class="smallIcon" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M2 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm3.75-1.5a.75.75 0 0 0 0 1.5h7.5a.75.75 0 0 0 0-1.5Zm0 4a.75.75 0 0 0 0 1.5h5.5a1 1 0 1 1 0 2H9.56a.5.5 0 0 1-.374-.832l1.72-1.868a.75.75 0 1 0-1.112-1.008l-2.063 2.24a2 2 0 0 0 1.49 3.468h1.69a2.5 2.5 0 1 0 0-5Zm-3.75 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2Z"></path>
-            </svg>
-          </button>
-          <button id="searchBtn" class="toolbarButton" title="Search">
-            <svg class="smallIcon" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M10.68 11.74a6 6 0 1 1 1.06-1.06l2.28 2.28a.75.75 0 1 1-1.06 1.06l-2.28-2.28ZM6.5 11A4.5 4.5 0 1 0 6.5 2a4.5 4.5 0 0 0 0 9Z"></path>
-            </svg>
-          </button>
-          <button id="foldAllBtn" class="toolbarButton" title="Fold All">
-            <svg class="smallIcon" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M0 2.75C0 1.784.784 1 1.75 1h12.5c.966 0 1.75.784 1.75 1.75v1.5a.75.75 0 0 1-1.5 0v-1.5a.25.25 0 0 0-.25-.25H1.75a.25.25 0 0 0-.25.25v10.5c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 14.25 15H1.75A1.75 1.75 0 0 1 0 13.25Z"></path>
-            </svg>
-          </button>
-          <button id="unfoldAllBtn" class="toolbarButton" title="Unfold All">
-            <svg class="smallIcon" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M0 2.75C0 1.784.784 1 1.75 1h12.5c.966 0 1.75.784 1.75 1.75v10.5A1.75 1.75 0 0 1 14.25 15H1.75A1.75 1.75 0 0 1 0 13.25Zm1.5 0a.25.25 0 0 1 .25-.25h12.5a.25.25 0 0 1 .25.25v10.5a.25.25 0 0 1-.25.25H1.75a.25.25 0 0 1-.25-.25Z"></path>
-            </svg>
-          </button>
-          <button id="fullscreenBtn" class="toolbarButton" title="Fullscreen">
-            <svg id="fullscreenIcon" class="smallIcon" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M3.75 2h2.5a.75.75 0 0 1 0 1.5h-2.5a.25.25 0 0 0-.25.25v2.5a.75.75 0 0 1-1.5 0v-2.5C2 2.784 2.784 2 3.75 2Zm6.5 0h2c.966 0 1.75.784 1.75 1.75v2a.75.75 0 0 1-1.5 0v-2a.25.25 0 0 0-.25-.25h-2a.75.75 0 0 1 0-1.5Zm-9 8.25a.75.75 0 0 1 .75.75v2a.25.25 0 0 0 .25.25h2.5a.75.75 0 0 1 0 1.5h-2.5A1.75 1.75 0 0 1 2 13.25v-2a.75.75 0 0 1 .75-.75Zm9 0a.75.75 0 0 1 .75-.75h2a.75.75 0 0 1 .75.75v2c0 .966-.784 1.75-1.75 1.75h-2a.75.75 0 0 1 0-1.5h2a.25.25 0 0 0 .25-.25v-2a.25.25 0 0 0-.25-.25h-2a.75.75 0 0 1-.75-.75Z"></path>
-            </svg>
-          </button>
-          <button id="formatCodeBtn" class="toolbarButton hide" title="Format">
-            <svg class="smallIcon" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M4.72 3.22a.75.75 0 0 1 1.06 1.06L3.06 7l2.72 2.72a.75.75 0 1 1-1.06 1.06L1.94 7.94a1.25 1.25 0 0 1 0-1.88Zm6.56 0a.75.75 0 0 0-1.06 1.06L12.94 7l-2.72 2.72a.75.75 0 1 0 1.06 1.06l2.78-2.78a1.25 1.25 0 0 0 0-1.88Z"></path>
-            </svg>
-          </button>
-          <button id="showInvisiblesBtn" class="toolbarButton" title="Show Invisibles">
-            <svg class="smallIcon" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M3.25 5.75a.75.75 0 0 1 0-1.5h9.5a.75.75 0 0 1 0 1.5ZM3 8a.75.75 0 0 1 .75-.75h8.5a.75.75 0 0 1 0 1.5h-8.5A.75.75 0 0 1 3 8Zm.75 2.75a.75.75 0 0 0 0 1.5h6.5a.75.75 0 0 0 0-1.5Z"></path>
-            </svg>
-          </button>
-        </div>
-      </div>
+<div class="container">
+<div class="fileHeader">
+  
+  <!-- Header Left - File Actions -->
+  <div class="fileActions">
+    <div class="fileNameDisplay">
+      <input type="text" id="fileNameInput" class="fileNameInput" value="" readonly spellcheck="false" />
     </div>
+    <button id="editSaveBtn" class="actionButton" title="Edit">
+      <svg id="editSaveIcon" class="icon" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M11.013 1.427a1.75 1.75 0 0 1 2.474 0l1.086 1.086a1.75 1.75 0 0 1 0 2.474l-8.61 8.61a1.75 1.75 0 0 1-.757.437l-3.26.88a.75.75 0 0 1-.918-.918l.88-3.26a1.75 1.75 0 0 1 .437-.757l8.61-8.61Zm1.414 1.06a.25.25 0 0 0-.354 0L11.26 3.3l1.44 1.44 1.113-1.113a.25.25 0 0 0 0-.354l-1.086-1.086Z"></path>
+      </svg>
+    </button>
+    <button id="cancelBtn" class="actionButton hide" title="Cancel">
+      <svg class="icon" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M3.72 3.72a.75.75 0 0 1 1.06 0L8 6.94l3.22-3.22a.75.75 0 1 1 1.06 1.06L9.06 8l3.22 3.22a.75.75 0 1 1-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 0 1-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 0 1 0-1.06Z"></path>
+      </svg>
+    </button>
+    <button id="copyBtn" class="actionButton" title="Copy">
+      <svg class="icon" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v6.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 15h-7.5A1.75 1.75 0 0 1 0 13.25v-6.5Zm5-5A1.75 1.75 0 0 0 3.25 3.5v6.5A1.75 1.75 0 0 0 5 11.75h7.5A1.75 1.75 0 0 0 14.25 10V3.5A1.75 1.75 0 0 0 12.5 1.75H5Zm.25 1.5a.25.25 0 0 0-.25.25v6.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-6.5a.25.25 0 0 0-.25-.25H5.25Z"></path>
+      </svg>
+    </button>
+    <button id="downloadBtn" class="actionButton" title="Download">
+      <svg class="icon" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M2.75 14A1.75 1.75 0 0 1 1 12.25v-2.5a.75.75 0 0 1 1.5 0v2.5c0 .138.112.25.25.25h10.5a.25.25 0 0 0 .25-.25v-2.5a.75.75 0 0 1 1.5 0v2.5A1.75 1.75 0 0 1 13.25 14H2.75Zm3.5-5.75a.75.75 0 0 1 1.5 0v-6.5a.75.75 0 0 1 1.5 0v6.5a.75.75 0 0 1 1.5 0l-2.25 2.5a.75.75 0 0 1-1.06 0l-2.24-2.5Z"></path>
+      </svg>
+    </button>
+  </div>
 
-
-  <!-- Coder  |  Component -->
-    <div id="coderWrapper">
-    
-      <div id="loadingSpinner" class="loading-spinner" data-active="false">
-        <div class="spinner-overlay">
-          <svg class="spinner-svg" viewBox="0 0 50 50">
-            <circle class="spinner-track" cx="25" cy="25" r="20"></circle>
-            <circle class="spinner-head" cx="25" cy="25" r="20"></circle>
+  <!-- Header Right - Toolbar (horizontally scrollable) -->
+  <div class="toolbarContainer">
+    <div class="toolbarGroup">
+      <button id="themeToggleBtn" class="toolbarButton" title="Toggle Theme">
+        <svg id="themeIcon" class="smallIcon" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M8 4a4 4 0 1 1 0 8 4 4 0 0 1 0-8Z"></path>
+        </svg>
+      </button>
+      <div class="fontSizeControl">
+        <button id="decreaseFontBtn" class="fontButton" title="Decrease Font Size">
+          <svg class="tinyIcon" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M3.75 7.25a.75.75 0 0 0 0 1.5h8.5a.75.75 0 0 0 0-1.5Z"></path>
           </svg>
-        </div>
+        </button>
+        <span id="fontSizeDisplay" class="fontSizeDisplay">12px</span>
+        <button id="increaseFontBtn" class="fontButton" title="Increase Font Size">
+          <svg class="tinyIcon" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M7.25 3.75a.75.75 0 0 1 1.5 0V7.25h3.5a.75.75 0 0 1 0 1.5h-3.5v3.5a.75.75 0 0 1-1.5 0v-3.5h-3.5a.75.75 0 0 1 0-1.5h3.5Z"></path>
+          </svg>
+        </button>
       </div>
-      
-  <!-- CodeMirror  |  Container -->
-      <div id="codeMirrorContainer"></div>
-      
+      <button id="wrapLinesBtn" class="toolbarButton" title="Wrap Lines">
+        <svg class="smallIcon" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M2 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm3.75-1.5a.75.75 0 0 0 0 1.5h7.5a.75.75 0 0 0 0-1.5Zm0 4a.75.75 0 0 0 0 1.5h5.5a1 1 0 1 1 0 2H9.56a.5.5 0 0 1-.374-.832l1.72-1.868a.75.75 0 1 0-1.112-1.008l-2.063 2.24a2 2 0 0 0 1.49 3.468h1.69a2.5 2.5 0 1 0 0-5Zm-3.75 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2Z"></path>
+        </svg>
+      </button>
+      <button id="searchBtn" class="toolbarButton" title="Search">
+        <svg class="smallIcon" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M10.68 11.74a6 6 0 1 1 1.06-1.06l2.28 2.28a.75.75 0 1 1-1.06 1.06l-2.28-2.28ZM6.5 11A4.5 4.5 0 1 0 6.5 2a4.5 4.5 0 0 0 0 9Z"></path>
+        </svg>
+      </button>
+      <button id="foldAllBtn" class="toolbarButton" title="Fold All">
+        <svg class="smallIcon" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M0 2.75C0 1.784.784 1 1.75 1h12.5c.966 0 1.75.784 1.75 1.75v1.5a.75.75 0 0 1-1.5 0v-1.5a.25.25 0 0 0-.25-.25H1.75a.25.25 0 0 0-.25.25v10.5c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 14.25 15H1.75A1.75 1.75 0 0 1 0 13.25Z"></path>
+        </svg>
+      </button>
+      <button id="unfoldAllBtn" class="toolbarButton" title="Unfold All">
+        <svg class="smallIcon" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M0 2.75C0 1.784.784 1 1.75 1h12.5c.966 0 1.75.784 1.75 1.75v10.5A1.75 1.75 0 0 1 14.25 15H1.75A1.75 1.75 0 0 1 0 13.25Zm1.5 0a.25.25 0 0 1 .25-.25h12.5a.25.25 0 0 1 .25.25v10.5a.25.25 0 0 1-.25.25H1.75a.25.25 0 0 1-.25-.25Z"></path>
+        </svg>
+      </button>
+      <button id="fullscreenBtn" class="toolbarButton" title="Fullscreen">
+        <svg id="fullscreenIcon" class="smallIcon" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M3.75 2h2.5a.75.75 0 0 1 0 1.5h-2.5a.25.25 0 0 0-.25.25v2.5a.75.75 0 0 1-1.5 0v-2.5C2 2.784 2.784 2 3.75 2Zm6.5 0h2c.966 0 1.75.784 1.75 1.75v2a.75.75 0 0 1-1.5 0v-2a.25.25 0 0 0-.25-.25h-2a.75.75 0 0 1 0-1.5Zm-9 8.25a.75.75 0 0 1 .75.75v2a.25.25 0 0 0 .25.25h2.5a.75.75 0 0 1 0 1.5h-2.5A1.75 1.75 0 0 1 2 13.25v-2a.75.75 0 0 1 .75-.75Zm9 0a.75.75 0 0 1 .75-.75h2a.75.75 0 0 1 .75.75v2c0 .966-.784 1.75-1.75 1.75h-2a.75.75 0 0 1 0-1.5h2a.25.25 0 0 0 .25-.25v-2a.25.25 0 0 0-.25-.25h-2a.75.75 0 0 1-.75-.75Z"></path>
+        </svg>
+      </button>
+      <button id="formatCodeBtn" class="toolbarButton hide" title="Format">
+        <svg class="smallIcon" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M4.72 3.22a.75.75 0 0 1 1.06 1.06L3.06 7l2.72 2.72a.75.75 0 1 1-1.06 1.06L1.94 7.94a1.25 1.25 0 0 1 0-1.88Zm6.56 0a.75.75 0 0 0-1.06 1.06L12.94 7l-2.72 2.72a.75.75 0 1 0 1.06 1.06l2.78-2.78a1.25 1.25 0 0 0 0-1.88Z"></path>
+        </svg>
+      </button>
+      <button id="showInvisiblesBtn" class="toolbarButton" title="Show Invisibles">
+        <svg class="smallIcon" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M3.25 5.75a.75.75 0 0 1 0-1.5h9.5a.75.75 0 0 1 0 1.5ZM3 8a.75.75 0 0 1 .75-.75h8.5a.75.75 0 0 1 0 1.5h-8.5A.75.75 0 0 1 3 8Zm.75 2.75a.75.75 0 0 0 0 1.5h6.5a.75.75 0 0 0 0-1.5Z"></path>
+        </svg>
+      </button>
     </div>
-    <div class="fileFooter">
-      <div class="footerStats">
-        <span id="fileLinesCount">0 lines</span><span class="footerDivider">•</span>
-        <span id="fileSize">0 KB</span><span class="footerDivider">•</span>
-        <span id="fileLanguageDisplay">Text</span>
-      </div>
-      
-  <!-- Footer  |  Right -->
-      <div class="footerRight">
-        <span id="cursorPosition" class="cursorInfo">Ln 1, Col 1</span><span class="footerDivider">•</span>
-        <span id="selectionInfo" class="selectionInfo"></span>
-        <span id="encodingDisplay" class="encodingInfo">UTF-8</span>
-        <span id="lastSavedIndicator" class="lastSavedIndicator"></span>
-      </div>
+  </div>
+</div>
+
+<!-- Coder | Component -->
+<div id="coderWrapper">
+  <div id="loadingSpinner" class="loading-spinner" data-active="false">
+    <div class="spinner-overlay">
+      <svg class="spinner-svg" viewBox="0 0 50 50">
+        <circle class="spinner-track" cx="25" cy="25" r="20"></circle>
+        <circle class="spinner-head" cx="25" cy="25" r="20"></circle>
+      </svg>
     </div>
-    
-  <!-- Commit  |  Save File -->
-    <div id="commitPanel" class="commitPanel hide">
-      <h3 class="panelTitle">Commit changes</h3>
-      <div class="panelContent">
-        <div><input type="text" id="commitTitleInput" class="commitInput" placeholder="Update filename.ext"/></div>
-        <div><textarea id="commitDescriptionInput" rows="4" class="commitTextarea" placeholder="Add an optional extended description..."></textarea></div>
-        <div class="panelButtons">
-          <button id="cancelEditBtn" class="secondaryButton">Cancel</button>
-          <button id="saveChangesBtn" class="primaryButton">Commit changes</button>
-        </div>
-      </div>
+  </div>
+  
+  <!-- CodeMirror | Container -->
+  <div id="codeMirrorContainer"></div>
+</div>
+
+<div class="fileFooter">
+  <div class="footerStats">
+    <span id="fileLinesCount">0 lines</span>
+    <span class="footerDivider">•</span>
+    <span id="fileSize">0 KB</span>
+    <span class="footerDivider">•</span>
+    <span id="fileLanguageDisplay">Text</span>
+  </div>
+  
+  <div class="footerRight">
+    <span id="cursorPosition" class="cursorInfo">Ln 1, Col 1</span>
+    <span class="footerDivider">•</span>
+    <span id="selectionInfo" class="selectionInfo"></span>
+    <span id="encodingDisplay" class="encodingInfo">UTF-8</span>
+  </div>
+</div>
+
+<!-- Search Panel -->
+<div id="searchPanel" class="searchPanel hide">
+  <div class="searchContent">
+    <input type="text" id="searchInput" class="searchInput" placeholder="Search..." autocomplete="off" />
+    <div class="searchControls">
+      <button id="searchPrevBtn" class="searchButton" title="Previous match">
+        <svg width="12" height="12" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"/>
+          <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8Zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Z"/>
+        </svg>
+      </button>
+      <button id="searchNextBtn" class="searchButton" title="Next match">
+        <svg width="12" height="12" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M6 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"/>
+          <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8Zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Z"/>
+        </svg>
+      </button>
+      <button id="closeSearchBtn" class="searchButton" title="Close search">
+        <svg width="12" height="12" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M3.72 3.72a.75.75 0 0 1 1.06 0L8 6.94l3.22-3.22a.75.75 0 1 1 1.06 1.06L9.06 8l3.22 3.22a.75.75 0 1 1-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 0 1-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 0 1 0-1.06Z"/>
+        </svg>
+      </button>
     </div>
-<!-- E N D -- E N D -- E N D ----------------------->
-    
-    
-    `;
-  }
+  </div>
+</div>
+
+<!-- Commit | Save File -->
+<div id="commitPanel" class="commitPanel hide">
+  <h3 class="panelTitle">Commit changes</h3>
+  <div class="panelContent">
+    <div class="commitField">
+      <input type="text" id="commitTitleInput" class="commitInput" placeholder="Update filename.ext" spellcheck="false" />
+    </div>
+    <div class="commitField">
+      <textarea id="commitDescriptionInput" rows="4" class="commitTextarea" placeholder="Add an optional extended description..." spellcheck="false"></textarea>
+    </div>
+    <div class="panelButtons">
+      <button id="cancelEditBtn" class="secondaryButton">Cancel</button>
+      <button id="saveChangesBtn" class="primaryButton">Commit changes</button>
+    </div>
+  </div>
+</div>
+<!-- END -- END -- END ----------------------->
+  `;
+}
 
   cacheElements() {
     this.elements = {
